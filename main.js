067022a79316a06a152412014f9b771681f63115
dev1d123 <dhuamanio@unsa.edu.pe>
2024-10-23 05:02:58 -0500

    Add jumping controls...run and jump doesn´t work correctly

--- main.js
+++ main.js
@@ -31,3 +31,41 @@ AFRAME.registerComponent('run-controls', {
         });
     }
 });
+
+//funcion para que el personaje salte al presiona la tecla space
+AFRAME.registerComponent('jump-controls', {
+    //definir propiedades del componente
+    schema: {
+        jumpHeight: {type: 'number', default: 5},
+        gravity: {type: 'number', default: -9.8}
+    },
+    init: function(){
+        this.velocityY = 0;
+        this.isJumping = false;
+        this.groundLevel = this.el.object3D.position.y;
+        this.jumpStartHeight = this.groundLevel;
+        var el = this.el;
+        window.addEventListener('keydown', (event) => {
+            if (event.code === 'Space' && !this.isJumping) {
+                console.log("saltando!!!");
+                this.isJumping = true;
+                this.velocityY = this.data.jumpHeight;
+            }
+        });
+    },
+    //funcion tick (ejecutar a cada frame)
+    tick: function(time, timeDelta){
+        var position = this.el.object3D.position;
+        console.log(position);
+        //si esta en el aire
+        if(this.isJumping){
+            position.y += this.velocityY * (timeDelta/1000); //bajar al jugador 
+            this.velocityY += this.data.gravity * (timeDelta/1000); //aumentar la gravedad
+            if (position.y <= this.groundLevel) {
+                position.y = this.groundLevel;
+                this.isJumping = false;
+                this.velocityY = 0;
+            }
+        }
+    }
+});
