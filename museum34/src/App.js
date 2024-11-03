cb322254d66a486be691d6fddddd07d903e8182a
dev1d123 <dhuamanio@unsa.edu.pe>
2024-11-02 22:30:43 -0500

    Show the new component

--- museum34/src/App.js
+++ museum34/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Logo from './components/Logo';
+import NavMenu from './components/NavMenu';
 function App() {
   return (
 
     <div>
       <Logo/>
+      <NavMenu/>
     </div>
+    
   );
 }
 
