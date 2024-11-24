6430ca51ecff815c0a56fe031021e4c9915de893
wcarlosc <wcarlosc@unsa.edu.pe>
2024-11-24 12:16:22 -0500

    eliminando modelos innecesarios, agregando un index en en models para facilitar la exportacion de modelos, agregrando photos de arequipa, agregando cielo de fondo

--- museum34/src/VRComponents/MuseumVirtual.js
+++ museum34/src/VRComponents/MuseumVirtual.js
@@ -8,6 +8,8 @@ import floor from './models/floor.obj';
 import floorTexture from './textures/floor.jpg';
 import floorNormalTexture from './textures/floor_normal.jpg';
 import mainMuseum from './models/main-museum.glb';
+import models from './models/';
+
 import main from './main.js';
 
 import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
@@ -16,13 +18,14 @@ import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
 import "aframe";
 import CamPos from "./CamPos.js";
 
+
 const MuseumVirtual = () => {
   const [isLoaded, setIsLoaded] = useState(false); // Estado para controlar si se cargan los recursos
 
   const handleLoadScene = () => {
     setIsLoaded(true);
   };
-
+  
   return (
     <div style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
       {isLoaded && <BottomMenu />} {}
@@ -82,18 +85,62 @@ const MuseumVirtual = () => {
             <img id="floor_normal-texture" src={floorNormalTexture}></img>
 
             <a-asset-item id="main-museum" src={mainMuseum}></a-asset-item>
+            <a-asset-item id="furina" src={models.furina}></a-asset-item>
+            <a-asset-item id="volcan" src={models.volcan}></a-asset-item>
+            <a-asset-item id="catedral" src={models.catedral}></a-asset-item>
+            <a-asset-item id="donkey_sillar_polycam" src={models.donkey_sillar_polycam}></a-asset-item>
+            <a-asset-item id="eagle_sillar_polycam" src={models.eagle_sillar_polycam}></a-asset-item>
+            
+            <a-asset-item id="podiums-obj" src={models.podiumsModel}></a-asset-item>
+            <img id="podiums-texture" src={models.podiumsTexture}></img>
+
+            <a-asset-item id="lamps-obj" src={models.lampsModel}></a-asset-item>
+            <img id="lamps-texture" src={models.lampsTexture}></img>
+
+            <a-asset-item id="recuadro-obj" src={models.recuadroModel}></a-asset-item>
+            <img id="moonlight-texture" src={models.moonlightTexture}></img>
+            <img id="catedralPhoto" src={models.catedralPhoto}></img>
+            <img id="volcanPhoto" src={models.volcanPhoto}></img>
+            <img id="miradorPhoto" src={models.miradorPhoto}></img>
+            <img id="volcanPhoto2" src={models.volcanPhoto2}></img>
+            <img id="plazaPhoto" src={models.plazaPhoto}></img>
+            <img id="sillarPhoto" src={models.sillarPhoto}></img>
+            <img id="rocotoPhoto" src={models.rocotoPhoto}></img>
+            
+
+
           </a-assets>
+          <a-sky src="#sky_sphere-texture"></a-sky>
+          
+          <Entity gltf-model="#main-museum" position="0 0 0" rotation="0 0 0" scale="1 1 1" static-body ></Entity>
+          <Entity gltf-model="#furina" position="0 0 0" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity gltf-model="#volcan" position="-6.5 0 -11" rotation="0 0 0" scale="0.05 0.05 0.05" static-body></Entity>
+          <Entity gltf-model="#catedral" position="10 1.8 -5" rotation="0 180 0" scale="3 3 3"static-body></Entity>
+          <Entity gltf-model="#donkey_sillar_polycam" position="9 1.18 7" rotation="0 0 0" scale="3 3 3" static-body></Entity>
+          <Entity gltf-model="#eagle_sillar_polycam" position="9.7 1.2 4" rotation="0 0 0" scale="2 2 2" static-body></Entity>
 
-          <a-sky color="#EEEEFF" material="src: #sky_sphere-texture"></a-sky>
+          {/* podiums */}
+          <Entity obj-model="obj: #podiums-obj" material="src: #podiums-texture" position="13 0 12.1" rotation="0 0 0" static-body></Entity>
+          <Entity obj-model="obj: #podiums-obj" material="src: #podiums-texture" position="10 0 12.1" rotation="0 0 0" static-body></Entity>
+          <Entity obj-model="obj: #podiums-obj" material="src: #podiums-texture" position="-13 0 12.1" rotation="0 0 0" static-body></Entity>
+          <Entity obj-model="obj: #podiums-obj" material="src: #podiums-texture" position="-10 0 12.1" rotation="0 0 0" static-body></Entity>
 
 
-          <Entity
-            gltf-model="#main-museum"
-            position="00 0 0"
-            rotation="0 0 0"
-            scale="1 1 1"
-            static-body
-          ></Entity>
+          {/* lamparas */}
+          <Entity obj-model="obj: #lamps-obj" material="src: #lamps-texture" position="12 0 -12" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity obj-model="obj: #lamps-obj" material="src: #lamps-texture" position="0 0 0" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity obj-model="obj: #lamps-obj" material="src: #lamps-texture" position="-12.7 0 -12" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity obj-model="obj: #lamps-obj" material="src: #lamps-texture" position="-10 0 -12" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+
+          {/* cuadros */}
+          <Entity obj-model="obj: #recuadro-obj" material="src: #moonlight-texture" position="-7.2 0 0" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity obj-model="obj: #recuadro-obj" material="src: #plazaPhoto" position="-6.2 -0.2  0" rotation="0 0 0" scale="0.7 1.1 1" static-body></Entity>
+          <Entity obj-model="obj: #recuadro-obj" material="src: #rocotoPhoto" position="18.8 -0.2 0" rotation="0 0 0" scale="1.21 1.1 1" static-body></Entity>
+          <Entity obj-model="obj: #recuadro-obj" material="src: #sillarPhoto" position="1 0 12" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity obj-model="obj: #recuadro-obj" material="src: #volcanPhoto2" position="3.3 0 12" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity obj-model="obj: #recuadro-obj" material="src: #catedralPhoto" position="5.6 0 12" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity obj-model="obj: #recuadro-obj" material="src: #volcanPhoto" position="7.9 0 12" rotation="0 0 0" scale="1 1 1" static-body></Entity>
+          <Entity obj-model="obj: #recuadro-obj" material="src: #miradorPhoto" position="10.2 0 12" rotation="0 0 0" scale="1 1 1" static-body></Entity>
 
           <Entity
             light="type: directional; color: #FFF; intensity: 0.5"
@@ -101,7 +148,8 @@ const MuseumVirtual = () => {
           ></Entity>
           <Entity light="type: ambient; color: #FFF"></Entity>
 
-          <a-box position="0 1 -5" color="red"></a-box>
+          {/* <a-box position="0 1 -5" color="red"></a-box> */}
+
           <a-light
             type="directional"
             position="2 4 -3"
@@ -109,7 +157,11 @@ const MuseumVirtual = () => {
             intensity="1"
           ></a-light>
 
+          {/* modelos */}
+          
+
           <a-entity id="player" camera look-controls="pointerLockEnabled: true" wasd-controls="acceleration: 35" run-controls jump-controls position="0 1.6 0"></a-entity>
+
 
         </Scene>
       )}
---
+++ museum34/src/VRComponents/models/index.js
@@ -0,0 +1,47 @@
+import furina from './genshin_impact_-_furina.glb';
+import volcan from './volcan_misti.glb'
+import podiumsModel from './podiums.obj';
+import podiumsTexture from '../textures/podiums.jpg';
+import lampsModel from './lamps.obj';
+import lampsTexture from '../textures/lamps.jpg';
+
+import recuadroModel from './recuadro.obj';
+import moonlightTexture from '../textures/moonlight.jpg';
+import catedralPhoto from '../textures/catedral.jpg'
+import volcanPhoto from '../textures/volcan.jpg'
+import miradorPhoto from '../textures/mirador.jpg'
+import sillarPhoto from '../textures/sillar.jpg'
+import rocotoPhoto from '../textures/rocoto.jpg'
+import plazaPhoto from '../textures/plaza.jpg'
+import volcanPhoto2 from '../textures/volcan_2.jpg'
+
+import donkey_sillar_polycam from './donkey_sillar_polycam.glb';
+import eagle_sillar_polycam from './eagle_sillar_polycam.glb';
+import catedral from './catedral.glb';
+
+
+
+
+const models = {  
+  furina,
+  volcan,
+  podiumsModel, podiumsTexture,
+  lampsModel, lampsTexture,
+  recuadroModel, 
+  moonlightTexture,
+  //Fotos
+  catedralPhoto,
+  volcanPhoto,
+  miradorPhoto,
+  sillarPhoto,
+  rocotoPhoto,
+  plazaPhoto,
+  volcanPhoto2,
+
+  donkey_sillar_polycam,
+  eagle_sillar_polycam,
+  catedral,
+  
+};
+
+export default models;
