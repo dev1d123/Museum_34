ca04d277b3e155f375a424079491c0408a167703
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-11-24 23:19:42 -0500

    walk sound effect added

--- museum34/src/VRComponents/MuseumVirtual.js
+++ museum34/src/VRComponents/MuseumVirtual.js
@@ -1,85 +1,97 @@
-import React, { useState } from "react";
-import {Entity, Scene} from 'aframe-react'
-import BottomMenu from './BottomMenu.js';
-
-import audio1 from './audio/アドリブ-_instrumental_.ogg';
-import skyTexture from './textures/sky_sphere.jpg';
-import floor from './models/floor.obj';
-import floorTexture from './textures/floor.jpg';
-import floorNormalTexture from './textures/floor_normal.jpg';
-import mainMuseum from './models/main-museum.glb';
-import main from './main.js';
-
-import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
-
-
-import "aframe";
-import CamPos from "./CamPos.js";
-import { div } from "@tensorflow/tfjs";
-
-// import para mostrar el modal
+import React, { useState, useEffect } from "react";
+import { Entity, Scene } from "aframe-react";
+import BottomMenu from "./BottomMenu.js";
+import stepSound from "./audio/step-sound.ogg";
+import audio1 from "./audio/アドリブ-_instrumental_.ogg";
+import skyTexture from "./textures/sky_sphere.jpg";
+import floor from "./models/floor.obj";
+import floorTexture from "./textures/floor.jpg";
+import floorNormalTexture from "./textures/floor_normal.jpg";
+import mainMuseum from "./models/main-museum.glb";
 import Modal3D from "../components/DataModels/ModalInformation.jsx";
+import "aframe";
+
+
 
 const MuseumVirtual = () => {
   const [isLoaded, setIsLoaded] = useState(false); // Estado para controlar si se cargan los recursos
-
-  // Estados para controlar el modal
   const [isModalOpen, setIsModalOpen] = useState(false); // Estado del modal
-  const [selectedModelId, setSelectedModelId] = useState(0); // ID del modelo
+
+  useEffect(() => {
+    let isMoving = false; // Controla si se está moviendo
+    const audio = new Audio(stepSound); // Cargar el sonido
+    audio.volume = 0.5;
+    audio.loop = true; // El sonido se repetirá
+    let keysPressed = new Set(); // Usaremos un Set para almacenar las teclas presionadas
+  
+    // Función para iniciar el sonido
+    const startSound = () => {
+      if (audio.paused) {
+        audio.play().catch((error) => console.error("Error playing sound:", error));
+      }
+    };
+  
+    // Función para detener el sonido
+    const stopSound = () => {
+      if (!audio.paused) {
+        audio.pause();
+        audio.currentTime = 0; // Reiniciar el sonido para la próxima vez
+      }
+    };
+  
+    // Manejadores de eventos para teclas de movimiento
+    const handleKeyDown = (event) => {
+      if (['w', 'a', 's', 'd'].includes(event.key.toLowerCase())) {
+        keysPressed.add(event.key.toLowerCase());
+        if (keysPressed.size > 0 && !isMoving) {
+          startSound();
+          isMoving = true;
+        }
+      }
+    };
+  
+    const handleKeyUp = (event) => {
+      if (['w', 'a', 's', 'd'].includes(event.key.toLowerCase())) {
+        keysPressed.delete(event.key.toLowerCase());
+        if (keysPressed.size === 0) {
+          isMoving = false;
+          stopSound();
+        }
+      }
+    };
+  
+    // Registrar eventos de teclado
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+  
+    // Cleanup al desmontar el componente
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+  
 
   const handleLoadScene = () => {
     setIsLoaded(true);
   };
 
-  // Funcion temporal para controlar el estado del modal
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
+
   return (
     <div style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
-      {isLoaded && <BottomMenu />} {}
+      {isLoaded && <BottomMenu />}
 
-      {/* 
-      {isLoaded && <CamPos />} {}
-      */}
-      {/* probando el modal */}
       {/* Modal para mostrar información */}
-      <Modal3D isOpen={isModalOpen} id={selectedModelId} onClose={() => setIsModalOpen(false)} />
-       {/* Botón superior para abrir/cerrar el modal */}
-       {isLoaded && (
-        <div
-          style={{
-            position: "absolute",
-            top: "10px",
-            right: "10px",
-            zIndex: 1000,
-            backgroundColor: "#fff",
-            padding: "10px",
-            borderRadius: "5px",
-            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <button
-            onClick={toggleModal}
-            style={{
-              padding: "10px 20px",
-              fontSize: "14px",
-              backgroundColor: isModalOpen ? "#FF5C5C" : "#6CEEB5",
-              color: "#fff",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-            }}
-          >
-            {isModalOpen ? "Cerrar Modal" : "Abrir Modal"}
-          </button>
-        </div>
-      )}
-
+      <Modal3D
+        isOpen={isModalOpen}
+        id={0} // Eliminé el uso de selectedModelId porque no se está usando
+        onClose={() => setIsModalOpen(false)}
+      />
 
       {!isLoaded ? (
-        // Pantalla inicial con botón para cargar la escena
-
         <div
           style={{
             display: "flex",
@@ -107,17 +119,14 @@ const MuseumVirtual = () => {
             Iniciar Experiencia
           </button>
         </div>
-
       ) : (
-        // La escena se carga después de hacer clic
-        
         <Scene>
           <a-assets>
             <a-mixin id="checkpoint"></a-mixin>
             <a-mixin id="checkpoint-hovered" color="#6CEEB5"></a-mixin>
 
-            <img id="sky_sphere-texture" src={skyTexture}></img>
-
+            <img id="sky_sphere-texture" src={skyTexture} alt="" />
+            
             <a-sound
               src={audio1}
               autoplay="true"
@@ -125,16 +134,14 @@ const MuseumVirtual = () => {
               position="1 1 0"
               volume="0.3"
             ></a-sound>
-
+            
             <a-asset-item id="floor-obj" src={floor}></a-asset-item>
-            <img id="floor-texture" src={floorTexture}></img>
-            <img id="floor_normal-texture" src={floorNormalTexture}></img>
-
+            <img id="floor-texture" src={floorTexture} alt="" />
+            <img id="floor_normal-texture" src={floorNormalTexture} alt="" />
             <a-asset-item id="main-museum" src={mainMuseum}></a-asset-item>
           </a-assets>
 
           <a-sky color="#EEEEFF" material="src: #sky_sphere-texture"></a-sky>
-
 
           <Entity
             gltf-model="#main-museum"
@@ -144,10 +151,7 @@ const MuseumVirtual = () => {
             static-body
           ></Entity>
 
-          <Entity
-            light="type: directional; color: #FFF; intensity: 0.5"
-            position="2 20 0"
-          ></Entity>
+          <Entity light="type: directional; color: #FFF; intensity: 0.5" position="2 20 0"></Entity>
           <Entity light="type: ambient; color: #FFF"></Entity>
 
           <a-box position="0 1 -5" color="red"></a-box>
@@ -158,8 +162,17 @@ const MuseumVirtual = () => {
             intensity="1"
           ></a-light>
 
-          <a-entity id="player" camera look-controls wasd-controls="acceleration: 35" run-controls jump-controls position="0 1.6 0"></a-entity>
-
+          {/* Entidad del jugador que dispara el sonido al moverse */}
+          <a-entity
+            id="player"
+            camera
+            look-controls
+            wasd-controls="acceleration: 35"
+            run-controls
+            jump-controls
+            position="0 1.6 0"
+            step-sound
+          ></a-entity>
         </Scene>
       )}
     </div>
